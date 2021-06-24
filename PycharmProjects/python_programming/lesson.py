54f4d35accf4fd937b48b206fe73c6f9f6a7d306
massive2351 <massive2351@gmail.com>
2021-06-24 16:06:46 +0900

    文字列のインデックスとスライス

--- PycharmProjects/python_programming/lesson.py
+++ PycharmProjects/python_programming/lesson.py
@@ -15,4 +15,22 @@ line2
 line3\
 """)
 print('###########')
+word = 'python'
+print(word[0])
+print(word[-1])
+print(word[2:5])
+print(word[:2])
+print(word[0:])
+
+word = "j" + word[1:]
+print(word)
+n = len(word)
+
+print(n)
+
+
+
+
+
+
 
