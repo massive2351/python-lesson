e0b201308185cda5fbc7ff97bc0f32a08957cfdf
massive2351 <massive2351@gmail.com>
2021-06-24 17:00:15 +0900

    タプルのアンパッキング

--- PycharmProjects/python_programming/lesson.py
+++ PycharmProjects/python_programming/lesson.py
@@ -1,6 +1,21 @@
-i = [1,2,3,4,5,1,2,3]
-j = i
-j[0] = 100
-print('j =',j)
-print('i =',i)
+num_tuple = (10,20)
+print(num_tuple)
 
+x,y = num_tuple
+print(x,y)
+
+x,y = 1000,20
+print(x,y)
+
+i = 10
+j = 20
+tmp = i
+i = j
+j = tmp
+
+print(i,j)
+
+a = 100
+b = 200
+a,b = b, a
+print(a,b)
