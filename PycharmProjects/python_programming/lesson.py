d136dd319102d3cb370d6366811721f94d930ff9
massive2351 <massive2351@gmail.com>
2021-06-24 16:45:23 +0900

    リストのコピー

--- PycharmProjects/python_programming/lesson.py
+++ PycharmProjects/python_programming/lesson.py
@@ -1,16 +1,6 @@
-r = [1,2,3,4,5,1,2,3]
+i = [1,2,3,4,5,1,2,3]
+j = i
+j[0] = 100
+print('j =',j)
+print('i =',i)
 
-print(r.index(3,3))
-print(r.count(3))
-
-if 100 in r:
-    print('exit')
-
-r.sort()
-print(r)
-
-s = 'My name is Mike.'
-to_split = s.split()
-print(to_split)
-
-print(help(list))
