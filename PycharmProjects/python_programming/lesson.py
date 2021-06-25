796b5439496c16b69d90c00a0cf09ec2c467c337
massive2351 <massive2351@gmail.com>
2021-06-25 21:17:08 +0900

    if文

--- PycharmProjects/python_programming/lesson.py
+++ PycharmProjects/python_programming/lesson.py
@@ -1,21 +1,10 @@
-num_tuple = (10,20)
-print(num_tuple)
+x = 10
 
-x,y = num_tuple
-print(x,y)
+if x < 0:
+    print('negative')
+else:
+    print('posi')
 
-x,y = 1000,20
-print(x,y)
 
-i = 10
-j = 20
-tmp = i
-i = j
-j = tmp
 
-print(i,j)
 
-a = 100
-b = 200
-a,b = b, a
-print(a,b)
