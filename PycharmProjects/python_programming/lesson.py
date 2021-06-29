2d8cd440c0948d5aa8ef2c534074022b02d9ef47
massive2351 <massive2351@gmail.com>
2021-06-29 17:54:08 +0900

    rang関数

--- PycharmProjects/python_programming/lesson.py
+++ PycharmProjects/python_programming/lesson.py
@@ -1,19 +1,7 @@
 
-some_list = [1, 2, 3, 4, 5]
 
-# i = 0
-# while i < len(some_list):
-#    print(some_list[i])
-#    i += 1
-
-for i in some_list:
+for i in range(2, 10, 3):
     print(i)
 
-for s in "abcde":
-    print(s)
-
-for word in ['My', 'name', 'is']:
-    print(word)
-
-
-
+for _ in range(10):
+    print('hellow')
