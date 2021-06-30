56d9df335b4dd28f3e1de68c8b96a3eaee692f71
massive2351 <massive2351@gmail.com>
2021-06-30 09:25:24 +0900

    位置引数

--- PycharmProjects/python_programming/lesson.py
+++ PycharmProjects/python_programming/lesson.py
@@ -1,16 +1,7 @@
-def say_something():
-    print('hi')
+def menu(entree, drink, desert):
+    print(entree)
+    print(drink)
+    print(desert)
 
-f = say_something()
-
-
-def what_is_this(color):
-    if color == 'red':
-        return 'tomato'
-    elif color == 'green':
-        return 'green peppar'
-    else:
-        return "i don't know"
-
-print(what_is_this('red'))
+menu('beef', 'juce', 'ice')
 
